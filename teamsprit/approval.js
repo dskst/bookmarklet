29e224b348476b7fc0caa394826029547dcd6322
dskst <10830352+dskst@users.noreply.github.com>
2020-10-02 11:36:52 +0900

    Add workflow approval

---
+++ teamsprit/approval.js
@@ -0,0 +1,31 @@
+javascript:(
+    function() {
+        var arrow_interval_id = setInterval(function() {
+            // click arrow
+            var arrow = document.querySelector('.rowActionsPlaceHolder.slds-button.slds-button--icon-x-small.slds-button--icon-border-filled.keyboardMode--trigger');
+            if (arrow) {
+                arrow.click();
+
+                // click approval dropdown
+                var dropdown_interval_id = setInterval(function() {
+                    var dropdown = document.querySelector('.forceActionLink');
+                    if (dropdown) {
+                        dropdown.click();
+                        clearInterval(dropdown_interval_id); 
+
+                        // click approval modal
+                        var modal_interval_id = setInterval(function() {
+                            var modal = document.querySelector('.slds-button.slds-button--neutral.modal-button-left.actionButton.uiButton--default.uiButton--brand.uiButton'); 
+                            if (modal) {
+                                modal.click();
+                                clearInterval(modal_interval_id);
+                            }
+                        }, 500);
+                    }
+                }, 500);
+            } else {
+                clearInterval(arrow_interval_id);
+            }
+        }, 500);
+    }
+)();
